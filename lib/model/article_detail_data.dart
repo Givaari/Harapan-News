bc48119c57ddc2bf5ab3139e54b0a2488cec1fd3
Givaari <givarijabaltorik@gmail.com>
2025-06-29 14:12:40 +0700

    first commit

--- lib/model/article_detail_data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:HarapanNews/model/news.dart';
-
-// Kelas ini berfungsi sebagai "bungkus" untuk dua jenis data
-class ArticleDetailData {
-  final News news;
-  final bool isBookmarked;
-
-  ArticleDetailData({required this.news, required this.isBookmarked});
-}
