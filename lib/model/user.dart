96da5d73f1b6d84ecfdb1f92638b359a6953e419
fikthekid <2206127@itg.ac.id>
2025-06-29 14:38:11 +0700

    news, user model

---
+++ lib/model/user.dart
@@ -0,0 +1,35 @@
+class User {
+  final String id;
+  final String email;
+  final String name;
+  final String? title;
+  final String? avatar;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.title,
+    this.avatar,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] as String,
+      email: json['email'] as String,
+      name: json['name'] as String,
+      title: json['title'] as String?,
+      avatar: json['avatar'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name,
+      'title': title,
+      'avatar': avatar,
+    };
+  }
+}
